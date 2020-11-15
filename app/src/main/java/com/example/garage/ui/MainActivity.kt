fafe5fddc2f12ed236a0595686cd3c98ea852d65
Aiperi <aip.amankulova@gmail.com>
2020-11-15 16:32:34 +0600

    New files

---
+++ app/src/main/java/com/example/garage/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.garage.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.garage.R
+
+class MainActivity : AppCompatActivity() {
+
+    private var host: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+        host = Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+}
